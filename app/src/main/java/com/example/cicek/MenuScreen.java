bd98b112966c0c6b76aaf21bb858a401150e59ac
ozturk sirin <ozturk2002.zri@gmail.com>
2022-10-22 14:29:28 +0300

    xml finish

--- app/src/main/java/com/example/cicek/MenuScreen.java
+++ app/src/main/java/com/example/cicek/MenuScreen.java
@@ -1,7 +1,6 @@
 package com.example.cicek;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MenuScreen extends AppCompatActivity {
--- app/src/main/java/com/example/cicek/SplashScreen.java
+++ app/src/main/java/com/example/cicek/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.cicek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+
+        Thread timerThread = new Thread(){ //timer kullanarak ekranın 5 saniye sonra diğer ekrana geçmesini söyliyorum
+            public void run(){
+                try{
+                    sleep(3000);//3sn olduğunu belirtiyorum
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this, MenuScreen.class);//ekran1'e geçmesini söylüyorum
+                    startActivity(intent);
+                }
+
+                finish();
+            }
+        };
+        timerThread.start();
+
+
     }
 }
