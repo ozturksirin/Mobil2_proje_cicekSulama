15c70de49044c687a1a2504d3886ea6844f90978
ozturk sirin <ozturk2002.zri@gmail.com>
2022-10-28 16:37:38 +0300

    Firebase Connection

--- app/src/main/java/com/example/cicek/MenuScreen.java
+++ app/src/main/java/com/example/cicek/MenuScreen.java
@@ -3,11 +3,20 @@ package com.example.cicek;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MenuScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
