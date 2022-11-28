251782a90c519eda7f5a4f335c2109e02f60ccec
ozturk sirin <ozturk2002.zri@gmail.com>
2022-11-28 21:15:53 +0300

    hakkinda screen

--- app/src/main/java/com/example/cicek/MenuScreen.java
+++ app/src/main/java/com/example/cicek/MenuScreen.java
@@ -1,17 +1,47 @@
 package com.example.cicek;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MenuScreen extends AppCompatActivity {
 
+    private Button btn_hakkinda;
+    private Button btn_iletisim;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+
+        btn_hakkinda = findViewById(R.id.btn_hakkinda);
+
+        btn_hakkinda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuScreen.this, HakkindaScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_iletisim = findViewById(R.id.btn_iletisim);
+
+        btn_iletisim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuScreen.this, IletisimScreen.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 /*
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
